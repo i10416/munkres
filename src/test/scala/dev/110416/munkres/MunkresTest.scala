d9b518f2138c734eeb8c0683d5cbe488d6abfd3d
ItoYo16u <ito.yo16uh90616@gmail.com>
2021-08-31 17:46:11 +0900

    add test case

--- src/test/scala/dev/110416/munkres/MunkresTest.scala
+++ src/test/scala/dev/110416/munkres/MunkresTest.scala
@@ -19,15 +19,40 @@ class MunkresTest extends AnyFlatSpec with Matchers:
       Array(373, 657, 227, 41, 57),
       Array(233, 527, 897, 301, 697)
     )
+    // 3 x 3
     val m3 = Array(
       Array(10.1, 10.2, 8.3),
       Array(9.4, 8.5, 1.6),
       Array(9.7, 7.8, 4.9)
     )
+    // 3 x 3
+    val m4 = Array(
+      Array(5,9,1),
+      Array(10,3,2),
+      Array(8,7,4)
+    )
+    // 3 x 3
+    val m5 = Array(
+      Array(5.1,9.2,1.3),
+      Array(10.4,3.5,2.6),
+      Array(8.7,7.8,4.9)
+    )
+
+    val m6 = Array(
+      Array(12,9,27,10,23),
+      Array(7,13,13,30,19),
+      Array(25,18,26,11,26),
+      Array(9,28,26,23,13),
+      Array(16,16,24,6,9)
+    )
+
     "Munkres.cost(nxn matrix)" should "calculate assignment cost" in {
         munkres.cost(m1) shouldBe 17.0
         munkres.cost(m2) shouldBe 1685
         munkres.cost(m3) shouldBe 19.5
+        munkres.cost(m4) shouldBe 12.0
+        munkres.cost(m5) shouldBe 13.5
+        munkres.cost(m6) shouldBe 51.0
     }
     val verticalRect = Array(
       Array(1.0, 1.0),
@@ -47,3 +72,4 @@ class MunkresTest extends AnyFlatSpec with Matchers:
         afterPadding2.length shouldBe 7
         afterPadding2.forall(row => row.length == 7) shouldBe true
     }
+
