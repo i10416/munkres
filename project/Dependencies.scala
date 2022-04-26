990af71dc42578e3e9bfced3745758581bb365a8
my-scala-steward <contact.110416+scalasteward@gmail.com>
2022-04-26 01:05:03 +0000

    Update scalatest to 3.2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-    val scalaTestVersion = "3.2.9"
+    val scalaTestVersion = "3.2.12"
     def deps = Seq(
       "org.scalatest" %% "scalatest" % scalaTestVersion % Test
     )
